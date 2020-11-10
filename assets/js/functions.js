9e89f1be17ba8437cd2de73c6bb8c8fe0b8ae518
Jonathan Haggard <info@jonathanhaggard.com>
2020-11-10 12:21:27 -0800

    dynamic text

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,6 +1,12 @@
 $( document ).ready(function() {
 
-  //
+  lottie.loadAnimation({
+  container: document.getElementById('lottie'), // the dom element that will contain the animation
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'assets/data.json' // the path to the animation json
+});
 
 });
 
